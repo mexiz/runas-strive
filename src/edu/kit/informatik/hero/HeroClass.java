5cbd493c5bdec453039803267dd3380791a7b534
Markus <uwlhp@student.kit.edu>
2022-03-27 13:41:48 +0200

    checkstyle

--- src/edu/kit/informatik/hero/HeroClass.java
+++ src/edu/kit/informatik/hero/HeroClass.java
@@ -4,10 +4,26 @@ import java.util.List;
 
 import edu.kit.informatik.model.Ability;
 
+/**
+ * Die Vorlage für die Klassen des Heldens
+ * 
+ * @author uwlhp
+ * @version 1.0.0
+ */
 public abstract class HeroClass {
 
-
+    /**
+     * Gibt die Fähigkeiten der Klasse aus
+     * 
+     * @param level das Level
+     * @return eine Liste mit den Fähigkeiten
+     */
     public abstract List<Ability> getAbilities(int level);
 
+    /**
+     * Gibt den Namen der Klasse des Heldens aus
+     * 
+     * @return den Namen
+     */
     public abstract String getHeroName();
 }
