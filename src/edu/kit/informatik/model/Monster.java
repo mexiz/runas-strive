37113f18d536bb048370ca04be12c0da07533e42
Markus <uwlhp@student.kit.edu>
2022-03-27 13:49:28 +0200

    checkstyle

--- src/edu/kit/informatik/model/Monster.java
+++ src/edu/kit/informatik/model/Monster.java
@@ -15,28 +15,33 @@ public abstract class Monster {
     private Ability prevAbility = new EmptyAbility();
 
     /**
-     * @return Ability
+     * Gibt die vorherige Fähigkeit aus
+     * 
+     * @return Ability die Fähigkeit
      */
     public Ability getPrevAbility() {
         return prevAbility;
     }
 
     /**
-     * @param prevAbility
+     * Setzt die vorherige Fähigkeit
+     * 
+     * @param prevAbility die zusetzende Fähigkeit
      */
     public void setPrevAbility(Ability prevAbility) {
         this.prevAbility = prevAbility;
     }
 
     /**
-     * @return boolean
+     * Abfrage ob der Monster tot ist
+     * 
+     * @return boolean true wenn das Monster tot ist
      */
     public boolean dead() {
         return this.getHealth() < 0;
     }
 
     /**
-     * 
      * Wenn die Karte eine magische Karte ist wird abgefragt ob der Monster genügend
      * Fokuspunkte hat
      * 
@@ -51,22 +56,66 @@ public abstract class Monster {
         return true;
     }
 
+    /**
+     * Ändert die Fähigkeit zur nächsten in der Liste
+     * 
+     */
     public abstract void changeAbility();
 
+    /**
+     * Setzt die aktuelle Fähikeit
+     * 
+     * @param abilities die Fähigkeit
+     */
     public abstract void setCurrentAbility(Ability abilities);
 
+    /**
+     * Gibt den Typ des Monsters zurück
+     * 
+     * @return den Typ des Monsters
+     */
     public abstract MonsterType getMonsterType();
 
+    /**
+     * Gibt die aktuelle Fähigkeit zurück
+     * 
+     * @return gibt die Fähigkeit
+     */
     public abstract Ability getCurrentAbility();
 
+    /**
+     * Gibt die Fokuspunkte zurück
+     * 
+     * @return die Fokuspunkte
+     */
     public abstract int getFocusPoints();
 
+    /**
+     * Setzt die Fokuspunkte
+     * 
+     * @param focusPoint die Fokuspunkte
+     */
     public abstract void setFocusPoints(int focusPoint);
 
+    /**
+     * Gitb die Leben zurück
+     * 
+     * @return die Leben
+     */
     public abstract int getHealth();
 
+    /**
+     * Setzt die Leben
+     * 
+     * @param health die Leben
+     */
     public abstract void setHealth(int health);
 
+    /**
+     * Gibt den Namen zurück
+     * 
+     * @return den Namen
+     */
     public abstract String getName();
 
 }
